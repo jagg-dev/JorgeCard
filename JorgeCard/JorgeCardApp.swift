e84940c3760d8f7a5f965e04627e3b7e072b339f
Jorge Gonzalez <jagg.dev.27@gmail.com>
2022-05-25 00:29:07 -0500

    Initial Commit

---
+++ JorgeCard/JorgeCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  JorgeCardApp.swift
+//  JorgeCard
+//
+//  Created by Jorge Gonzalez on 25/05/22.
+//
+
+import SwiftUI
+
+@main
+struct JorgeCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
